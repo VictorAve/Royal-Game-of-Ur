eea698cfacd06c511bc2066b40f5aaa780ab7b2a
VictorAve <50817105+VictorAve@users.noreply.github.com>
2021-12-30 07:06:26 +1300

    Initial creation of files
    Created Royal Game of Ur, Stone, Board, Dice classes.
    Dice class is (hopefully) complete.

---
+++ Dice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Royal_Game_of_Ur
+{
+    class Dice
+    {
+        Random rnd = new Random();
+        int rollOne()
+        {
+            int result = this.rnd.Next(0, 1);
+            return result;
+        }
+
+        int[] rollAll()
+        {
+            int[] results = new int[5];
+            int sum = 0;
+            for (int i = 0; i < results.Length - 1; i++)
+            {
+                results[i] = this.rollOne();
+                sum += results[i];
+            }
+            results[4] = sum;
+            return results;
+        }
+    }
+}
---
+++ Royal Game of Ur.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Royal Game of Ur
+ * Victor Ferreira van Eyk
+ * 30/12/2021
+ * 
+ * This is an ascii form of the Royal Game of Ur
+ * This is my first foray into C#
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Royal_Game_of_Ur
+{
+    public class RoyalGameOfUr
+    {
+        static void Main(string[] args)
+        {
+            Dice myDice = new Dice();
+            Stone[] playerOneStones = new Stone[5];
+            Stone[] playerTwoStones = new Stone[5];
+            Board myBoard = new Board();
+        }
+    }
+}
