4d87cfbbedbd40d49685d9998957a77b828f2820
VictorAve <50817105+VictorAve@users.noreply.github.com>
2022-01-01 15:55:19 +1300

    Creation of the Display class
    Contains the printBoard method. Relies on the contained getVisual method.

---
+++ Display.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Royal_Game_of_Ur
+{
+    internal class Display
+    {
+        public void printBoard(Board myBoard)
+        {
+            Console.WriteLine("P1:[0000000]      Score:[       ]");
+            Console.WriteLine("+---+---+---+---+       +---+---+");
+            Console.WriteLine("| {0} | {1} | {2} | {3} |       | {4} | {5} |",
+                getVisual("*", myBoard.P1Row[3]), getVisual(" ", myBoard.P1Row[2]), getVisual(" ", myBoard.P1Row[1]), 
+                getVisual(" ", myBoard.P1Row[0]), getVisual("*", myBoard.P1Row[5]), getVisual(" ", myBoard.P1Row[4]));
+            Console.WriteLine("+---+---+---+---+---+---+---+---+");
+            Console.WriteLine("| {0} | {1} | {2} | {3} | {4} | {5} | {6} | {7} |", 
+                getVisual(" ", myBoard.centerRow[0]), getVisual(" ", myBoard.centerRow[1]), getVisual(" ", myBoard.centerRow[2]), 
+                getVisual("*", myBoard.centerRow[3]), getVisual(" ", myBoard.centerRow[4]), getVisual(" ", myBoard.centerRow[5]), 
+                getVisual(" ", myBoard.centerRow[6]), getVisual(" ", myBoard.centerRow[7]));
+            Console.WriteLine("+---+---+---+---+---+---+---+---+");
+            Console.WriteLine("| {0} | {1} | {2} | {3} |       | {4} | {5} |",
+                getVisual("*", myBoard.P2Row[3]), getVisual(" ", myBoard.P2Row[2]), getVisual(" ", myBoard.P2Row[1]),
+                getVisual(" ", myBoard.P2Row[0]), getVisual("*", myBoard.P2Row[5]), getVisual(" ", myBoard.P2Row[4]));
+            Console.WriteLine("+---+---+---+---+       +---+---+");
+            Console.WriteLine("P2:[0000000]      Score:[       ]");
+        }
+
+        string getVisual(string baseSymbol, Tile myTile)
+        {
+            string newSymbol = myTile.getisOccupied() ? myTile.getOwner():baseSymbol;
+            return newSymbol;
+        }
+    }
+}
