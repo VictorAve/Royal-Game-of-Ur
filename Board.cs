6e437b62a671a04dfe048bbf4c628859e9fd6dfc
VictorAve <50817105+VictorAve@users.noreply.github.com>
2022-01-01 15:53:21 +1300

    Initialisation of the Board
    Includes a method to initialise the Board

--- Board.cs
+++ Board.cs
@@ -6,7 +6,52 @@ using System.Threading.Tasks;
 
 namespace Royal_Game_of_Ur
 {
-    internal class Board
+    class Board
     {
+
+        public Tile[] P1Row = new Tile[6];
+        public Tile[] P2Row = new Tile[6];
+        public Tile[] centerRow = new Tile[8];
+        public Tile[][] boardState = new Tile[3][];
+        public void initialise()
+        {
+            Tile thisTile;
+            for (int i = 0; i < P1Row.Length; i++)
+            {
+                P1Row[i] = new Tile();
+                thisTile = P1Row[i];
+                if (i == 3)
+                {
+                    thisTile.setSpecial();
+                }
+                P1Row[i] = thisTile;
+            }
+            boardState[0] = P1Row;
+
+            for (int i = 0; i < centerRow.Length; i++)
+            {
+                centerRow[i] = new Tile();
+                thisTile = centerRow[i];
+                if (i == 3)
+                {
+                    thisTile.setSpecial();
+                }
+                centerRow[i] = thisTile;
+            }
+            boardState[0] = centerRow;
+
+            for (int i = 0; i < P2Row.Length; i++)
+            {
+                P2Row[i] = new Tile();
+                thisTile = P2Row[i];
+                if (i == 3)
+                {
+                    thisTile.setSpecial();
+                }
+                P2Row[i] = thisTile;
+            }
+            boardState[0] = P2Row;
+        }
+
     }
 }
