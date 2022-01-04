24e7b4f84eaa54a1cdee9d946ba3f79ca2908148
VictorAve <50817105+VictorAve@users.noreply.github.com>
2022-01-04 14:35:13 +1300

    Displayed board now has tile numbers

--- Display.cs
+++ Display.cs
@@ -10,22 +10,24 @@ namespace Royal_Game_of_Ur
     {
         public void printBoard(Board myBoard)
         {
-            Console.WriteLine("P1:[0000000]      Score:[       ]");
-            Console.WriteLine("+---+---+---+---+       +---+---+");
-            Console.WriteLine("| {3} | {2} | {1} | {0} |       | {5} | {4} |",
+            Console.WriteLine("      1   2   3   4   5   6   7   8  ");
+            Console.WriteLine();
+            Console.WriteLine("    P1:[0000000]      Score:[       ]");
+            Console.WriteLine("    +---+---+---+---+       +---+---+");
+            Console.WriteLine("a  | {3} | {2} | {1} | {0} |       | {5} | {4} |",
                 getVisual(" ", myBoard.P1Row[0]), getVisual(" ", myBoard.P1Row[1]), getVisual(" ", myBoard.P1Row[2]), 
                 getVisual("*", myBoard.P1Row[3]), getVisual(" ", myBoard.P1Row[4]), getVisual("*", myBoard.P1Row[5]));
-            Console.WriteLine("+---+---+---+---+---+---+---+---+");
-            Console.WriteLine("| {0} | {1} | {2} | {3} | {4} | {5} | {6} | {7} |", 
+            Console.WriteLine("    +---+---+---+---+---+---+---+---+");
+            Console.WriteLine("b  | {0} | {1} | {2} | {3} | {4} | {5} | {6} | {7} |", 
                 getVisual(" ", myBoard.centerRow[0]), getVisual(" ", myBoard.centerRow[1]), getVisual(" ", myBoard.centerRow[2]), 
                 getVisual("*", myBoard.centerRow[3]), getVisual(" ", myBoard.centerRow[4]), getVisual(" ", myBoard.centerRow[5]), 
                 getVisual(" ", myBoard.centerRow[6]), getVisual(" ", myBoard.centerRow[7]));
-            Console.WriteLine("+---+---+---+---+---+---+---+---+");
-            Console.WriteLine("| {3} | {2} | {1} | {0} |       | {5} | {4} |",
+            Console.WriteLine("    +---+---+---+---+---+---+---+---+");
+            Console.WriteLine("c  | {3} | {2} | {1} | {0} |       | {5} | {4} |",
                 getVisual(" ", myBoard.P2Row[0]), getVisual(" ", myBoard.P2Row[1]), getVisual(" ", myBoard.P2Row[2]),
                 getVisual("*", myBoard.P2Row[3]), getVisual(" ", myBoard.P2Row[4]), getVisual("*", myBoard.P2Row[5]));
-            Console.WriteLine("+---+---+---+---+       +---+---+");
-            Console.WriteLine("P2:[0000000]      Score:[       ]");
+            Console.WriteLine("    +---+---+---+---+       +---+---+");
+            Console.WriteLine("    P2:[0000000]      Score:[       ]");
         }
 
         string getVisual(string baseSymbol, Tile myTile)
